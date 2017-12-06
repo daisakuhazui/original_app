ad8368a47fab38a0f9558bda05c4dfcfaed676dc
筈井大策 <daisakuhazui@Vino-MAC.local>
2017-12-06 09:45:21 +0900

    Userモデルの作成
    deviseを使ってuserモデルを作成しました。また、genre_usersテーブルを通してgenresテーブルと関連付けを行っています。

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :genres, through: :genre_users
+  has_many :genre_users
+end
