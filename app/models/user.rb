cf2e4afa5062a0c3ae75fcf79467935e5b20cf81
筈井大策 <daisakuhazui@Vino-MAC.local>
2017-12-09 23:08:34 +0900

    親要素が削除されたら子要素も削除されるようにする
    オプションを追加し、userが削除されればgenre_userも削除されるようにしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :genres, through: :genre_users
-  has_many :genre_users
+  has_many :genre_users :dependent => :destroy
 end
