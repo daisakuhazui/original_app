5be84717281e584ac58db0122159fa8894414fea
筈井大策 <daisakuhazui@Vino-MAC.local>
2017-12-09 23:16:49 +0900

    dependentオプションのsyntax error修正
    " , "が抜けていたため追記しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :genres, through: :genre_users
-  has_many :genre_users :dependent => :destroy
+  has_many :genre_users, :dependent => :destroy
 end
