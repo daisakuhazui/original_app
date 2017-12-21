fef7bbe6f28b5a40ee5bd33d4b01fed819bf0235
筈井大策 <daisakuhazui@Vino-MAC.local>
2017-12-21 20:18:56 +0900

    before_actionの設定
    各アクションにあった同じコードを1箇所にまとめました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def show
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       # この後、updateが成功したらflashメッセージが出るようにする
     else
@@ -13,12 +13,15 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
 private
 
     def user_params
       params.require(:user).permit(:email, :alias_name, :name, :profile_image, :cover_image, :area, :introduce )
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
