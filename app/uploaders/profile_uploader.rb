50a7baa79d25505beceb248fa1b7a7163cf4c147
筈井大策 <daisakuhazui@Vino-MAC.local>
2017-12-31 00:05:33 +0900

    配列の丸括弧とシングルコーテーションの修正

--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -1,13 +1,13 @@
 class ProfileUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   process resize_to_limit: [200, 200]
-  process convert: 'jpg'
+  process convert: "jpg"
 
   def extension_white_list
-    %w(jpg jpeg gif png)
+    %w[jpg jpeg gif png]
   end
 
   def filename
-    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+    super.chomp(File.extname(super)) + ".jpg" if original_filename.present?
   end
 end
