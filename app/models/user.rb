90efe9716d2ea9fd3cc34644a8a0bb1038fabad0
筈井大策 <daisakuhazui@Vino-MAC.local>
2017-12-30 23:48:09 +0900

    Userモデルへinverse_of:オプションの追加
    UserモデルとGenreモデルの間のデータの不整合が生じなくなるようにしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :genres, through: :genre_users
+  has_many :genres, through: :genre_users, inverse_of: :user
   has_many :genre_users, dependent: :destroy
 
   mount_uploader :profile_image, ProfileUploader
