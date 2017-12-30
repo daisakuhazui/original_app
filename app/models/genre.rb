41b69d2fd9bfb81b58f5efd9961fe040ac0a2de8
筈井大策 <daisakuhazui@Vino-MAC.local>
2017-12-30 23:49:47 +0900

    Genreモデルへのinverse_ofオプションの追加
    GenreモデルとGenre_userモデルの間でデータの不整合が生じないようにしました。

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,4 @@
 class Genre < ApplicationRecord
-  has_many :users, through: :genre_users
-  has_many :genre_users
+  has_many :users, through: :genre_users, inverse_of: :genre
+  has_many :genre_users, dependent: :nullify
 end
