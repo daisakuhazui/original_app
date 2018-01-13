3e6d921843b5326886e5d2b074afdc5565b85bc9
筈井大策 <daisakuhazui@Vino-MAC.local>
2018-01-13 22:13:07 +0900

    フォロー機能関連のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get "follows/create"
+  get "follows/destroy"
   resources :questions, only: [:index]
   root to: "questions#index"
   devise_for :users
   resources :users, only: [:show, :update, :edit]
+  resources :follows
 end
