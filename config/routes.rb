a02b97a14d7cb00bfc53cf45ca5949f503e1eddb
筈井大策 <daisakuhazui@Vino-MAC.local>
2018-01-14 07:43:14 +0900

    ルーティングの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get "follows/create"
-  get "follows/destroy"
   resources :questions, only: [:index]
   root to: "questions#index"
   devise_for :users
   resources :users, only: [:show, :update, :edit]
-  resources :follows
+  resources :follows, only: [:index, :create, :destroy]
 end
