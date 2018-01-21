a2c9b503de92b671f6167a44fa319ea92d0dcfd4
筈井大策 <daisakuhazui@Vino-MAC.local>
2018-01-21 18:16:22 +0900

    アクションの追加
    質問の投稿・削除に関するアクションを追加しました。
    private以下に定義したcorrect_userメソッドは、ユーザーが自分の投稿以外を削除できないようにしています。

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,39 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, :set_question
+  before_action :correct_user, only: :destroy
+
   def index
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:success] = "Question created!"
+      redirect_to root_url
+    else
+      render 'questions/index'
+    end
+  end
+
+  def destroy
+    @question.destroy
+    flash[:success] = "Question deleted"
+    redirect_to request.referrer || root_url
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:content, :user_id)
+  end
+
+  def set_question
+    @user = User.find(current_user.id)
+    @question = current_user.questions.build(user_id: @user.id)
+  end
+
+  def correct_user
+    @question = current_user.questions.find_by(id: params[:id])
+    redirect_to root_url if @question.nil?
   end
 end
