6d2183c7f5054e16fd640c7fa395d331504f6b02
筈井大策 <daisakuhazui@Vino-MAC.local>
2018-01-25 09:15:46 +0900

    不要なスペースを削除

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
   has_many :pictures, as: :imageable, dependent: :destroy
-    default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :desc) }
 end
