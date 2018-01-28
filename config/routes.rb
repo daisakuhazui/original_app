db0ed89df96b4f15f46b32a9bfa9967e7bbf8be7
筈井大策 <daisakuhazui@Vino-MAC.local>
2018-01-28 18:34:44 +0900

    ルーティングの調整
    不要だったshowを削除し、destroyを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
     end
   end
   resources :follows, only: [:index, :create, :destroy]
-  resources :answers, only: [:new, :create, :show]
+  resources :answers, only: [:new, :create, :destroy]
 end
